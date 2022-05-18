b7d46262b014fda9e5333738a6e98ed8cf8bd166
Zhou He Sheng <hszhou@ilegendsoft.com>
2022-05-18 14:17:43 +0800

    androidx

--- app/src/main/java/com/example/recyclerviewitemfocus/ItemListAdapter.kt
+++ app/src/main/java/com/example/recyclerviewitemfocus/ItemListAdapter.kt
@@ -1,15 +1,17 @@
 package com.example.recyclerviewitemfocus
 
-import android.support.v7.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 
-class ItemListAdapter(private val itemList: List<String>) : RecyclerView.Adapter<ItemListAdapter.ViewHolder>() {
+class ItemListAdapter(private val itemList: List<String>) :
+    RecyclerView.Adapter<ItemListAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutView = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        val layoutView =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
         return ViewHolder(layoutView)
     }
 
@@ -19,15 +21,21 @@ class ItemListAdapter(private val itemList: List<String>) : RecyclerView.Adapter
 
     override fun getItemCount() = itemList.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
         var tvTopic: TextView
 
         init {
             itemView.setOnClickListener(this)
             tvTopic = itemView.findViewById<View>(R.id.topic) as TextView
         }
+
         override fun onClick(view: View) {
-            Toast.makeText(view.context, "Clicked Position = " + adapterPosition, Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                view.context,
+                "Clicked Position = " + adapterPosition,
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
--- app/src/main/java/com/example/recyclerviewitemfocus/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewitemfocus/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.recyclerviewitemfocus
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +11,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val topics = listOf("Education","Finance","Government","Entertainment","Technology","Math","Biology","Physics","Chemistry","Space","Sports","Music","Animal","Countries","Weather","Politics","Traffic","Poverty","Social Media","Internet","Housing")
+        val topics = listOf(
+            "Education",
+            "Finance",
+            "Government",
+            "Entertainment",
+            "Technology",
+            "Math",
+            "Biology",
+            "Physics",
+            "Chemistry",
+            "Space",
+            "Sports",
+            "Music",
+            "Animal",
+            "Countries",
+            "Weather",
+            "Politics",
+            "Traffic",
+            "Poverty",
+            "Social Media",
+            "Internet",
+            "Housing"
+        )
         val linearLayoutManager = LinearLayoutManager(this)
         val listAdapter = ItemListAdapter(topics)
 
